ca4290e46b0a67f016f18ee0f8ef7d4209670cc5
grey cashmere <dobsonemily@gmail.com>
2021-04-14 15:37:50 -0700

    adding files and starting routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,30 @@
+
+// const { model } = require("mongoose");
+
+const router = require ("express").Router();
+//api routes
+
+router.get ()
+router.put ()
+router.post ()
+router.get ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports= router; 
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express= require("express");
-const mongoose = require("monogoose");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const PORT = process.env.PORT || 8080;
+const db = require("/models");
 
-const PORT= process.env.PORT || 3000;
-
-const db= require("/models");
 
 const app = express();
+app.use (logger("dev")); 
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(require ("./routes"));
+
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
