04846815412a36b2d041d11a69b62ea8cc9e6950
grey cashmere <dobsonemily@gmail.com>
2021-04-15 20:48:09 -0700

    formatted documents

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,72 +1,68 @@
 // const db = require("./models");
-const router = require ("express").Router();
+const router = require("express").Router();
 
-const { Workout }= require ("../models");
-
+const { Workout } = require("../models");
 
 //api/workouts
 
-router.get ("/workouts", async function(req, res){
-    try{
-const data = await Workout.aggregate ([
-{
-    $addFields:{
-        totalDuration:{
-            $sum:"$exercises.duration",
+router.get("/workouts", async function (req, res) {
+  try {
+    const data = await Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration",
+          },
         },
-    },
-},
-]);
-res.json(data);
-}catch(err){
+      },
+    ]);
+    res.json(data);
+  } catch (err) {
     console.log(err);
     res.status(500).send(err);
-}
+  }
 });
 
 //api/workouts/:id
-router.put ("/workouts/:id", async function(req, res){
-try{  
- const data= await Workout.updateOne({
-_id: req.params.id 
-}, {
-$push: {
-    exercises: req.body
-}
-},)
-res.json(data);
-}catch (err){
-    console.log(err)
-    res.json(err)
-}
-}); 
+router.put("/workouts/:id", async function (req, res) {
+  try {
+    const data = await Workout.updateOne(
+      {
+        _id: req.params.id,
+      },
+      {
+        $push: {
+          exercises: req.body,
+        },
+      }
+    );
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.json(err);
+  }
+});
 
 //api/workouts
-router.post ("/workouts", function(req, res){
-Workout.create(req.body)
-.then(data => {
-res.json(data);
-}) .catch (err => {
-    console.log(err)
-})
-
+router.post("/workouts", function (req, res) {
+  Workout.create(req.body)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 //api/workouts/range
-router.get ("/workouts/range", async function(req, res){
-    try{
-const data= await Workout.find()
-res.json(data);
-}catch (err){
-    console.log(err)
-  res.json(err)   
-}
+router.get("/workouts/range", async function (req, res) {
+  try {
+    const data = await Workout.find();
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.json(err);
+  }
+});
 
-})
-
-
-
-
-
-
-module.exports= router; 
+module.exports = router;
